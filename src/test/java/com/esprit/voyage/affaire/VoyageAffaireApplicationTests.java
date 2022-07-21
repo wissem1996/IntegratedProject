fd1e978e3ff5a83484a70efc811433319074201b
wissem1996 <wghrissi5@gmail.com>
2022-07-21 14:25:55 +0100

    adding tests

---
+++ src/test/java/com/esprit/voyage/affaire/VoyageAffaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.voyage.affaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageAffaireApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
